7c6f8e73c47ae2091641c53e2b35257d45b8e982
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-08-03 14:58:05 +0530

    Not provided

--- app/src/main/java/com/bunty/library/ui/search/SearchFragment.java
+++ app/src/main/java/com/bunty/library/ui/search/SearchFragment.java
@@ -1,15 +1,12 @@
 package com.bunty.library.ui.search;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.bunty.library.R;
-import com.bunty.library.databinding.FragmentProfileBinding;
+import androidx.fragment.app.Fragment;
+
 import com.bunty.library.databinding.FragmentSearchBinding;
 
 
